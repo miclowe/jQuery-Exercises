a0cf42bd594042fc1616dc44055314475f0e1906
Michelle Lowe <mick_571@yahoo.com>
2013-04-29 23:21:51 -0700

    Add styling and delete fadeOut

--- homework/ToDoList/js/todo.js
+++ homework/ToDoList/js/todo.js
@@ -1,42 +1,28 @@
 $(document).ready(function() {
-  var count = 1;
   $('#addBtn').click(function() {
     
+    var deleteButton = "<button class='deleteBtn'><img src='images/remove-icon.png'><span class='btnText'>Remove</span></button>"
     var newItem = $('#addItemTxt').val();
-    var todoItem = $("<li><input type='checkbox' id='item" + count + "'>" + newItem + "<button class='deleteBtn'><img src='images/remove-icon.png'><span class='btnText'>Remove</span></button></li>");
-
-    todoItem.appendTo("#todoList");
+    var todoItem = $("<li><input type='checkbox'>" + newItem + deleteButton + "</li>");
+    todoItem.appendTo("#todoItems");
     $('#addItemTxt').val('');
-    count += 1;
 
     $('input:checkbox').click(function(){
-      var cb = $(this);
-      if (cb.is(':checked')) {
-        cb.parent().appendTo('#completed');
-        cb.parent().css('color', 'red');
-        cb.parent().css('text-decoration', 'line-through');
+      var chkBox = $(this);
+      if (chkBox.is(':checked')) {
+        chkBox.parent().appendTo('#completedItems');
+        chkBox.parent().css('color', 'red');
+        chkBox.parent().css('text-decoration', 'line-through');
       } else {
-        cb.parent().appendTo('#todoList');
-        cb.parent().css('color', 'black');
-        cb.parent().css('text-decoration', 'none');
-        
+        chkBox.parent().appendTo('#todoItems');
+        chkBox.parent().css('color', 'black');
+        chkBox.parent().css('text-decoration', 'none');        
       }
     })
 
     $('.deleteBtn').click(function(){
       var btn = $(this);
-      btn.parent().remove();
+      btn.parent().fadeOut(800, function() {btn.parent().remove()});
     })
-
-
-
-    // todoItem.click(function() {
-    //   var item = $(this);
-    //   // $('input[name=' + newItem + ']').remove();
-    //   item.appendTo("#completed");
-    //   item.css("color", "red");
-    //   item.css("text-decoration", "line-through");
-    // })
   })
 });
-
