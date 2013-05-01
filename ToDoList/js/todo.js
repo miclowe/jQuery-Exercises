78cba64b45e7837b6e255c0ba27af82f574329e9
Michelle Lowe <mick_571@yahoo.com>
2013-04-30 21:00:03 -0700

    Refactored to make more obj oriented

--- ToDoList/js/todo.js
+++ ToDoList/js/todo.js
@@ -1,24 +1,29 @@
 $(document).ready(function() {
-  $('#addBtn').click(function() {
-    
-    var deleteButton = "<button class='deleteBtn'><img src='images/remove-icon.png'><span class='btnText'>Remove</span></button>"
-    var newItem = $('#addItemTxt').val();
-    var todoItem = $("<li><input type='checkbox'>" + newItem + deleteButton + "</li>");
-    todoItem.appendTo("#todoItems");
-    $('#addItemTxt').val('');
 
-    $('input:checkbox').click(function(){
-      var chkBox = $(this);
+$("#addBtn").on("click", function(){
+  var newItem = $("<span>" + $('#addItemTxt').val() + "</span>");
+  var listTags = $("<li></li>");
+  var checkBox = $("<input type='checkbox'>");
+  var deleteImg = $("<button class='deleteBtn'><img class='delete' src='images/remove-icon.png'><span class='btnText'>Remove</span></button>");
+
+  deleteImg.on("click", function(){
+    var btn = $(this);
+    btn.parent().fadeOut(800, function() {btn.parent().remove()});
+  });
+
+  checkBox.on("click", function(){
+    var chkBox = $(this);
       if (chkBox.is(':checked')) {
         chkBox.parent().appendTo('#completedItems').addClass('completed');
       } else {
         chkBox.parent().appendTo('#todoItems').removeClass('completed');
       }
-    })
+    });
 
-    $('.deleteBtn').click(function(){
-      var btn = $(this);
-      btn.parent().fadeOut(800, function() {btn.parent().remove()});
-    })
-  })
-});
+  $(checkBox).appendTo(listTags);
+  $(newItem).appendTo(listTags);
+  $(deleteImg).appendTo(listTags);
+  $(listTags).appendTo('#todoItems');
+  $('#addItemTxt').val('');
+  });
+ });
